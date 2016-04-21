d801714292eb3443c703c159a77c037b393690e9
hydroboy407 <MacBookPro@Charleys-MacBook-Pro.local>
2016-04-21 17:09:06 -0400

    here

---
+++ headers/addrecordform.h
@@ -0,0 +1,23 @@
+#ifndef ADDRECORDFORM_H
+#define ADDRECORDFORM_H
+
+#include <QDialog>
+
+namespace Ui {
+class addRecordForm;
+}
+
+class addRecordForm : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit addRecordForm(QWidget *parent = 0);
+    ~addRecordForm();asdasdasda
+    //stuffasdasdasdasd
+
+private:
+    Ui::addRecordForm *ui;
+};
+
+#endif // ADDRECORDFORM_H
---
+++ headers/databaseform.h
@@ -0,0 +1,23 @@
+#ifndef DATABASEFORM_H
+#define DATABASEFORM_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class DatabaseForm;
+}
+
+class DatabaseForm : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit DatabaseForm(QWidget *parent = 0);
+    ~DatabaseForm();asdfadsfadsfadsfadsfadsfadsfadsfadsfads
+    //stuuf
+
+private:
+    Ui::DatabaseForm *ui;
+};
+
+#endif // DATABASEFORM_H
